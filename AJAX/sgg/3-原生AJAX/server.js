5a1dd124289cc9271684094120cf315a2b77f0e3
zou-VUE <zoudecai@sz99.com>
2022-02-15 19:17:15 +0800

    fix:更新AJAX内容

--- AJAX/sgg/3-原生AJAX/server.js
+++ AJAX/sgg/3-原生AJAX/server.js
@@ -11,9 +11,51 @@ const app = express();
 app.get('/server', (request, response) => {
   // 设置响应头   设置允许跨域
   response.setHeader('Access-Control-Allow-Origin', '*');
-
   // 设置响应体
   response.send('HELLO AJAX');
+})
+
+// 可以接收任意类型的请求
+app.all('/server', (request, response) => {
+  // 设置响应头   设置允许跨域
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  // 响应头
+  response.setHeader('Access-Control-Allow-Headers', '*');
+  // 设置响应体
+  response.send('HELLO AJAX POST');
+})
+
+app.all('/json-server', (request, response) => {
+  // 设置响应头   设置允许跨域
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  // 响应头
+  response.setHeader('Access-Control-Allow-Headers', '*');
+  const data = {
+    name: 'atguigu'
+  };
+  // 对对象进行字符串转换
+  let str = JSON.stringify(data)
+  // 设置响应体
+  response.send(str);
+})
+
+// 针对IE缓存
+app.get('/ie', (request, response) => {
+  // 设置响应头   设置允许跨域
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  // 设置响应体
+  response.send('HELLO IE');
+})
+
+
+// 延时响应
+app.get('/delay', (request, response) => {
+  // 设置响应头   设置允许跨域
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  setTimeout(() => {
+    // 设置响应体
+    response.send('延时响应');
+  }, 3000)
 })
 
 // 4.监听端口启动服务
